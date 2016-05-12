8132284666d4d829bd8610b51f90057b6a0024dd
Hassan Ali <helfi92@gmail.com>
2016-05-12 19:14:55 -0400

    add charts for pressures and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,13 +7,15 @@ class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+
 		return (
 			<tr key={name}>
 				<td>{name}</td>
-			
-				<td>
-					<Chart color="red" data={temps} />
-				</td>
+				<td><Chart color="red" data={temps} /></td>
+				<td><Chart color="green" data={pressures} /></td>
+				<td><Chart color="black" data={humidities} /></td>
 			</tr>
 		)
 	}
